65b68c364789f0a2301d55e63051ad2fd9182511
vasil <vasil.bazhiv@global-main.com>
2023-12-25 16:43:41 +0200

    mini settings

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,13 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
-const About: NextPage = () => <div> hello!</div>;
+const About: NextPage = ({}): JSX.Element => (
+  <>
+    <Head>
+      <title>About</title>
+    </Head>
+    <div> hello!</div>
+  </>
+);
 
 export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title></title>
+        <title>Test Project</title>
       </Head>
       <main>
         <h1>
